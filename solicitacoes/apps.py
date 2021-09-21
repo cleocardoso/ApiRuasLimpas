535250f12beeded735eae90ea6b40691242f0088
cleocardoso <cleotads21@gmail.com>
2021-09-21 20:49:23 -0300

    ajuste na config...

--- solicitacoes/apps.py
+++ solicitacoes/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class SolicitacoesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'solicitacoes'
+    label = 'solicitacoes'
