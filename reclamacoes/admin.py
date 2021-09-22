a7cac2919c818bb8df62b73aa759c88908a076bb
cleocardoso <cleotads21@gmail.com>
2021-09-21 21:23:12 -0300

    ajuste na config...

--- reclamacoes/admin.py
+++ reclamacoes/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from solicitacoes.models import solicitacoes
-from .models import *
+from .models import Reclamacoes, Categoria
 # Register your models here.
 admin.site.register(Reclamacoes)
 admin.site.register(Categoria)
