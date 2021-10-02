4cfe6799c29a5878dc75f72ca862ba832175bdcd
cleocardoso <cleotads21@gmail.com>
2021-10-02 15:18:37 -0300

    ajustes no listagem

--- RuasLimpas/config/base.py
+++ RuasLimpas/config/base.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'lz!_#eyc487pt9^6g3i(5u%xfe&kg)z^1bo%5*6-&lo)3$y!@h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['apiruaslimpas.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'usuarios',
     'reclamacoes',
-
+    'solicitacoes',
 ]
 
 MIDDLEWARE = [
